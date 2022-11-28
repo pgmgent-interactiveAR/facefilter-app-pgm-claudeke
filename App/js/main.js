49deffe2fde11712f1f4e9c931d55aa7ef421251
pgm-claudeke <claudine.dekesel@student.arteveldehs.be>
2022-11-28 12:08:38 +0100

    added color vars and styling splash and btns

--- App/js/main.js
+++ App/js/main.js
@@ -3,14 +3,17 @@
         init() {
             this.cacheElements();
             this.splashToFilter();
-
+            this.showFilters();
+            this.hideFilter()
         },
         cacheElements() {
             this.$splashPage = document.querySelector('.splash');
             this.$startBtn = document.querySelector('.btn--start');
             this.$filtersPage = document.querySelector('.filters');
 
-            this.$showFiltersBtn = document.querySelector('.show-filters');
+            this.$showFiltersBtn = document.querySelector('.btn--up');
+            this.filtersBox = document.querySelector('.filters-box');
+            this.$hideFiltersBtn = document.querySelector('.btn--down');
         },
         splashToFilter() {
             this.$startBtn.addEventListener('click', (ev) => {
@@ -20,7 +23,14 @@
         },
         showFilters() {
             this.$showFiltersBtn.addEventListener('click', (ev) => {
-                
+                this.filtersBox.classList.remove('filters-box--hide');
+                this.$showFiltersBtn.classList.add('btn--hide');
+            })
+        },
+        hideFilter() {
+            this.$hideFiltersBtn.addEventListener('click', (ev) => {
+                this.filtersBox.classList.add('filters-box--hide');
+                this.$showFiltersBtn.classList.remove('btn--hide');
             })
         }
     }
