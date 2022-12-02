fa5dd6e27acf1c4032d043df4329ef147a9d7136
pgm-claudeke <claudine.dekesel@student.arteveldehs.be>
2022-12-02 11:41:01 +0100

    added models en working btns and animations

--- App/js/main.js
+++ App/js/main.js
@@ -4,7 +4,8 @@
             this.cacheElements();
             this.splashToFilter();
             this.showFilters();
-            this.hideFilter()
+            this.hideFilter();
+            this.selectFilter();
         },
         cacheElements() {
             this.$splashPage = document.querySelector('.splash');
@@ -12,8 +13,12 @@
             this.$filtersPage = document.querySelector('.filters');
 
             this.$showFiltersBtn = document.querySelector('.btn--up');
-            this.filtersBox = document.querySelector('.filters-box');
+            this.$filtersBox = document.querySelector('.filters-box');
             this.$hideFiltersBtn = document.querySelector('.btn--down');
+
+            this.$filterBtns = document.querySelectorAll('.btn--filter');
+
+            this.$allModels = document.querySelectorAll('.m');
         },
         splashToFilter() {
             this.$startBtn.addEventListener('click', (ev) => {
@@ -21,18 +26,103 @@
                 this.$filtersPage.classList.remove('filters--hide');
             });
         },
-        showFilters() {
+        showFilters() {mustache
             this.$showFiltersBtn.addEventListener('click', (ev) => {
-                this.filtersBox.classList.remove('filters-box--hide');
+                this.$filtersBox.classList.remove('filters-box--hide');
                 this.$showFiltersBtn.classList.add('btn--hide');
             })
         },
         hideFilter() {
             this.$hideFiltersBtn.addEventListener('click', (ev) => {
-                this.filtersBox.classList.add('filters-box--hide');
+                this.$filtersBox.classList.add('filters-box--hide');
                 this.$showFiltersBtn.classList.remove('btn--hide');
             })
-        }
+        },
+        selectFilter() {
+            this.$filterBtns.forEach(btn => {
+                this.selectFilterFunction(btn);
+                
+            });
+
+        },
+        selectFilterFunction(btn) {
+            btn.addEventListener('click', (ev) => {
+                if (btn.classList.contains('btn--selected')) {
+                    btn.classList.remove('btn--selected');
+                    this.$allModels.forEach(model => {
+                        if (model.classList.contains(btn.value)) {
+                            model.setAttribute('scale', {
+                                x: 0,
+                                y: 0,
+                                z: 0,
+                            });
+                        }
+                    })
+                } else {
+                    btn.classList.add('btn--selected');
+                    this.$allModels.forEach(model => {
+                        if (model.classList.contains(btn.value)) {
+                            this.setScaling(model)
+                        }
+                    })
+                }
+            });
+        },
+        setScaling(model) {
+           
+            if (model.classList.contains('m-glasses')) {
+                model.setAttribute('animation', { 'property': 'scale',
+                'to': {
+                    x: 0.04,
+                    y: 0.04,
+                    z: 0.04
+                }, 
+                'dur': 300,
+                'startEvents': 'startanim001',
+                });
+            } else if (model.classList.contains('m-shat') || model.classList.contains('m-ehat')) {
+                model.setAttribute('animation', { 'property': 'scale',
+                'to': {
+                    x: 0.125,
+                    y: 0.125,
+                    z: 0.125
+                },
+                'dur': 300,
+                'startEvents': 'startanim001',
+                });
+            } else if(model.classList.contains('m-mustache')) {
+                model.setAttribute('animation', { 'property': 'scale',
+                'to': {
+                    x: 0.01,
+                    y: 0.01,
+                    z: 0.01
+                },
+                'dur': 300,
+                'startEvents': 'startanim001',
+                });
+            } else if(model.classList.contains('m-nose')) {
+                model.setAttribute('animation', { 'property': 'scale',
+                'to': {
+                    x: 0.07,
+                    y: 0.07,
+                    z: 0.07
+                },
+                'dur': 300,
+                'startEvents': 'startanim001',
+                });
+            } else if(model.classList.contains('m-beard')) {
+                model.setAttribute('animation', { 'property': 'scale',
+                'to': {
+                    x: 0.004,
+                    y: 0.004,
+                    z: 0.004
+                },
+                'dur': 300,
+                'startEvents': 'startanim001',
+                });
+            }
+            model.emit(`startanim001`, null, false)
+        },
     }
     app.init();
 })();
